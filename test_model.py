be27f7e2d9f7c168e63f9d9501cff4e6d061550a
GofiveDDD <149077105+GofiveDDD@users.noreply.github.com>
2024-05-14 19:02:07 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -21,6 +21,56 @@ class TestDataLoaderAndSplitter(unittest.TestCase):
             self.images, self.labels, test_size=0.2, random_state=42)
         self.assertEqual(train_images.shape[0], len(train_labels))
         self.assertEqual(validation_images.shape[0], len(validation_labels))
+        
+    def test_model_structure(self):
+        model = Sequential([
+            Input(shape=(28, 28, 1)),
+            MyConv2D(filters=32, kernel_size=5),
+            MaxPooling2D(pool_size=(2, 2)),
+            MyConv2D(filters=64, kernel_size=5),
+            MaxPooling2D(pool_size=(2, 2)),
+            Flatten(),
+            Dense(units=1024, activation='relu'),
+            Dropout(0.5),
+            Dense(units=10, activation='softmax')
+        ])
+        self.assertEqual(len(model.layers), 8)
+
+    def test_model_compilation(self):
+        model = Sequential([
+            Input(shape=(28, 28, 1)),
+            MyConv2D(filters=32, kernel_size=5),
+            MaxPooling2D(pool_size=(2, 2)),
+            MyConv2D(filters=64, kernel_size=5),
+            MaxPooling2D(pool_size=(2, 2)),
+            Flatten(),
+            Dense(units=1024, activation='relu'),
+            Dropout(0.5),
+            Dense(units=10, activation='softmax')
+        ])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.assertEqual(model.optimizer.__class__.__name__, 'Adam')
+
+    def test_model_training(self):
+        images = np.random.rand(1000, 28, 28, 1)
+        labels = to_categorical(np.random.randint(0, 10, size=1000), num_classes=10)
+        
+        model = Sequential([
+            Input(shape=(28, 28, 1)),
+            MyConv2D(filters=32, kernel_size=5),
+            MaxPooling2D(pool_size=(2, 2)),
+            MyConv2D(filters=64, kernel_size=5),
+            MaxPooling2D(pool_size=(2, 2)),
+            Flatten(),
+            Dense(units=1024, activation='relu'),
+            Dropout(0.5),
+            Dense(units=10, activation='softmax')
+        ])
+        
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        history = model.fit(images, labels, epochs=2, batch_size=32, validation_split=0.2, verbose=0)
+        self.assertEqual(len(history.history['loss']), 2)
 
 if __name__ == '__main__':
     unittest.main()
